d0d6f0a26c72f63fadc0fef82bf8313ff4cfbb99
davidaknowles <knowles84@gmail.com>
2014-05-07 15:30:57 -0700

    Added differential ASE test example

--- dASE_test.R
+++ dASE_test.R
@@ -1,4 +1,6 @@
 # This is a test script for the Environment ASE GLMM
+# In this case the model is used to test for differential ASE rather than association
+# with an environment variable. 
 # Data sampled from the generative model and then we run inference to attempt to
 # recover the true "hits"
 
@@ -11,9 +13,10 @@ alt=list() # list of alternative counts
 n=list() # list of total counts
 xFull=list() # list of design matrices for the alternative models
 xNull=list() # list of design matrices for the null models
-environmentVar=rnorm(n.samples) # values of the environment variable, e.g. age
 # make "true" regression coefficients, most of which are 0
-trueBeta=ifelse(runif(n.loci) < 0.05, rgamma(n.loci,shape=2,rate=1), 0.0) 
+
+trueBeta=ifelse(runif(n.loci) < 0.05, rgamma(n.loci,shape=2,scale=.2)*(1-2*(runif(n.loci)<0.5)), 0.0)
+
 logistic=function(x) 1/(1+exp(-x))
 for (i in 1:n.loci){
     maf=runif(1)*.4+.1 # MAF between .1 and .5 for this locus
@@ -23,11 +26,10 @@ for (i in 1:n.loci){
     hets=xor(hap1,hap2)
     numHets=sum(hets)
     ones=numeric(numHets)+1.0 
-    x=environmentVar[hets]
-    xFull[[i]]=cbind(x,ones) # design matrix at this locus for alternative ("full") model
-    xNull[[i]]=cbind(ones) # design matrix at this locus for null model
+    xFull[[i]]=cbind(ones) # design matrix at this locus for alternative ("full") model
+    xNull[[i]]=matrix(NA,ncol=0,nrow=numHets) # design matrix at this locus for null model
     n[[i]]=rpois(numHets,100*rgamma(numHets,shape=2,rate=2)) # sample read depth from overdispersed Poisson (NB?)
-    p=logistic(trueBeta[i]*x+.3*rnorm(numHets)) # sample underlying probabilities, with overdispersion
+    p=logistic(trueBeta[i]+.3*rnorm(numHets)) # sample underlying probabilities, with overdispersion
     alt[[i]]=rbinom(numHets,n[[i]],p) # sample alternative counts
     #for (j in 1:numHets) # "min" model: doesn't work well with the way I sample betas here
     #    alt[[i]][j]=min(alt[[i]][j],n[[i]][j]-alt[[i]][j])
