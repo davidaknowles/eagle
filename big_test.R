21fca3602baf83f89edd9f2b485839a19e7680d0
David Knowles <davidknowles@DN0a2305c9.SUNet>
2015-08-27 17:57:46 -0700

    Added eQTLs to test script

--- big_test.R
+++ big_test.R
@@ -6,14 +6,16 @@ require(eagle) # load the package
 set.seed(1) # for reproducibility
 
 #-------------- generate some synthetic data -------------------------
-n.loci=1000 # number of loci
-n.samples=1000 # number of individuals
+n.loci=300 # number of loci
+n.samples=200 # number of individuals
 alt=matrix(NA,n.samples,n.loci) # matrix of alternative counts
 totalreads=matrix(NA,n.samples,n.loci) # matrix of total counts
 het=matrix(NA,n.samples,n.loci) # matrix of which individuals are hets at each locus
-environmentVar=rnorm(n.samples) # values of the environment variable, e.g. age
+environmentVar=runif(n.samples) # values of the environment variable, e.g. age
+eqtlGenotypes=matrix(NA,n.samples,n.loci) # genotypes (0,1,2) of the lead eQTL for each gene
 # make "true" regression coefficients, most of which are 0
-trueBeta=ifelse(runif(n.loci) < 0.05, rgamma(n.loci,shape=2,rate=1), 0.0) 
+#trueBeta=ifelse(runif(n.loci) < 0.05, rgamma(n.loci,shape=5,rate=5), 0.0) 
+trueBeta=ifelse(runif(n.loci) < 0.05, runif(n.loci), 0.0) 
 logistic=function(x) 1/(1+exp(-x))
 for (i in 1:n.loci){
     maf=runif(1)*.4+.1 # MAF between .1 and .5 for this locus
@@ -22,7 +24,15 @@ for (i in 1:n.loci){
     hap2=runif(n.samples)<maf
     het[,i]=xor(hap1,hap2)
     totalreads[,i]=rpois(n.samples,100*rgamma(n.samples,shape=2,rate=2)) # sample read depth from overdispersed Poisson (NB?)
-    p=logistic(trueBeta[i]*environmentVar+.3*rnorm(n.samples)) # sample underlying probabilities, with overdispersion
+    logOdds=.3+trueBeta[i]*environmentVar+.3*rnorm(n.samples)
+    if (runif(1)<.5){ # add eQTL
+      eSNPmaf=runif(1)*.4+.1 
+      ehap1=runif(n.samples)<eSNPmaf 
+      ehap2=runif(n.samples)<eSNPmaf
+      eqtlGenotypes[,i]=ehap1+ehap2
+      logOdds=logOdds+.3*(eqtlGenotypes[,i]==1)
+    }
+    p=logistic(logOdds) # sample underlying probabilities, with overdispersion
     alt[,i]=rbinom(n.samples,totalreads[,i],p) # sample alternative counts
 }
 alt[het!=1]=NA
@@ -46,7 +56,7 @@ for (snp.index in 1:n.loci) {
     next
   a=alt[valid,snp.index]
   r=ref[valid,snp.index]
-  #heteq= t(eqtls[snp.index,valid]==1)
+  heteq= eqtlGenotypes[valid,snp.index]==1
   x=environmentVar[valid]
   # check not too many are in one group (e.g. female, non-smokers)
   if ((length(x)-max(table(x)))<10) 
@@ -57,19 +67,36 @@ for (snp.index in 1:n.loci) {
   is.mono=(min.a.r<count.cutoff)|((as.double(min.a.r)/n)<prop.cutoff)
   if (mean(is.mono)>prop.mono.cutoff)
     next
-  alt.list[[non.problematic.counter]]=a
+  alt.list[[non.problematic.counter]]=pmin(a,n-a) # min model
   n.list[[non.problematic.counter]]=n
   original.index[[non.problematic.counter]]=snp.index
   num.samples=length(x)
   x.full[[non.problematic.counter]]=cbind(env=x,intercept=numeric(num.samples)+1.0)
   x.null[[non.problematic.counter]]=cbind(intercept=numeric(num.samples)+1.0)
-  #if ((!any(is.na(heteq))) & (sd(heteq)>0) & (min(table(heteq))>5)){ # last two are probably redundant
-  #  x.full[[non.problematic.counter]]=cbind(x.full[[non.problematic.counter]],eqtl=heteq)
-  #  x.null[[non.problematic.counter]]=cbind(x.null[[non.problematic.counter]],eqtl=heteq)
-  #}
+  # add eQTL heterozygosity
+  if (!any(is.na(heteq))) if ((sd(heteq)>0) & (min(table(heteq))>5)){ 
+    x.full[[non.problematic.counter]]=cbind(x.full[[non.problematic.counter]],eqtl=heteq)
+    x.null[[non.problematic.counter]]=cbind(x.null[[non.problematic.counter]],eqtl=heteq)
+    
+    # lots of conditions to decide whether to add an interaction terms
+    if (min(table(heteq))>=10) { # only if at least 10 hets and 10 non-hets
+      e1=x[heteq==1.0]
+      e0=x[heteq==0.0]
+      if ((length(e1)-max(table(e1)))>=10 & (length(e0)-max(table(e0)))>=10){ 
+        if (length(unique(x))>2 | min(table(x,heteq))>=5){ 
+          temp=cbind(x.full[[non.problematic.counter]],interaction=x*heteq)
+          if (det(t(temp) %*% temp) > 1e-8)
+            x.full[[non.problematic.counter]]=temp
+        }
+      }
+    }
+    
+  }
   non.problematic.counter=non.problematic.counter+1
 }
 original.index=unlist(original.index)
+
+trueBetaAtTested=trueBeta[original.index]
 
 #---------------- run the model --------------------------
 s=eagle.settings()
@@ -80,9 +107,8 @@ s$normalised.depth=scale(log10(unlist(lapply(n,mean)))) # only required for rev.
 s$max.iterations=10000
 s$convergence.tolerance=.001
 s$coeff.regulariser=0.0
-s$learn.rev=T
 
-# initial hyperparamters
+# learnt parameters from the DGN dataset
 s$rep.global.shape=1.0
 s$rep.global.rate=0.0033
 
@@ -90,10 +116,10 @@ s$traceEvery=1
 
 system.time( res <- eagle.helper(alt.list,n.list,x.full,x.null,s) ) # 4s
 
-trueBetaAtTested=trueBeta[original.index]
-
-# save(file="testNoRerun.RData",res,trueBeta)
 cat("p-values for true hits:",res$p.values[trueBetaAtTested!=0],"\n")
 cat("true hit betas: ",trueBetaAtTested[trueBetaAtTested!=0],"\n")
 
 hist( res$p.values[ trueBetaAtTested==0.0 ], main="p values for null sites")
+
+i=which.max(trueBetaAtTested)
+plot(x.full[[i]][,1],alt.list[[i]]/n.list[[i]],ylim=c(0,1))
